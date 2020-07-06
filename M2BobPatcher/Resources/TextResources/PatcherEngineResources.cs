5cb6a7bcf9c50c149d642a541148011c12c81d85
Igor Ruivo <ia.ruivo@campus.fct.unl.pt>
2020-07-06 18:19:56 +0100

    Engine pipeline rework.

--- M2BobPatcher/Resources/TextResources/PatcherEngineResources.cs
+++ M2BobPatcher/Resources/TextResources/PatcherEngineResources.cs
@@ -1,10 +1,10 @@
 ﻿namespace M2BobPatcher.Resources.TextResources {
     public static class PatcherEngineResources {
-        public static readonly string DOWNLOADING_SERVER_METADATA = "Step 1 of 5: Downloading server metadata.";
-        public static readonly string DOWNLOADING_OUTDATED_CONTENT = "Step 5 of 5: Downloading outdated content.";
-        public static readonly string DOWNLOADING_MISSING_CONTENT = "Step 3 of 5: Downloading missing content.";
-        public static readonly string GENERATING_LOCAL_METADATA = "Step 4 of 5: Generating local metadata.";
-        public static readonly string PARSING_SERVER_METADATA = "Step 2 of 5: Parsing server metadata.";
+        public static readonly string STEP = "Step {0} of {1}: ";
+        public static readonly string DOWNLOADING_OUTDATED_CONTENT = "Downloading outdated content.";
+        public static readonly string DOWNLOADING_MISSING_CONTENT = "Downloading missing content.";
+        public static readonly string GENERATING_LOCAL_METADATA = "Generating local metadata.";
+        public static readonly string PARSING_SERVER_METADATA = "Parsing server metadata.";
         public static readonly string FINISHED = "Everything up to date. You can now start M2Bob!";
         public static readonly string ALL_FILES_ANALYZED = "All files analyzed in {0}.";
     }
