763edea70491698b6ccf7fb64d3d8de7d9a38cc9
igor-ruivo <igor.ruivo.97@gmail.com>
2020-07-10 13:24:22 +0100

    Adding previously deleted sensitive information.

---
+++ M2BobPatcher/Resources/Configs/EngineConfigs.cs
@@ -0,0 +1,16 @@
+﻿namespace M2BobPatcher.Resources.Configs {
+    /// <summary>
+    /// The class with the required configuration to be used by the engine.
+    /// </summary>
+    public static class EngineConfigs {
+        /// <summary>
+        /// The link to M2Bob's metadata file.
+        /// </summary>
+        public static readonly string M2BOB_PATCH_METADATA = "http://your-server/patchlist.txt";
+
+        /// <summary>
+        /// The time (in milliseconds) that the engine will spend sleeping before performing any consistency checks.
+        /// </summary>
+        public static readonly int MS_TO_WAIT_FOR_AV_FALSE_POSITIVES = 1000;
+    }
+}
---
+++ M2BobPatcher/Resources/TextResources/MainWindowResources.cs
@@ -0,0 +1,41 @@
+﻿namespace M2BobPatcher.Resources.TextResources {
+    /// <summary>
+    /// The class with the text resources of all main window's textual fields.
+    /// </summary>
+    public static class MainWindowResources {
+        /// <summary>
+        /// The M2Bob main website.
+        /// </summary>
+        public static readonly string M2BOB_WEBSITE = "https://your-website/";
+
+        /// <summary>
+        /// The author main website.
+        /// </summary>
+        public static readonly string AUTHOR_WEBSITE = "https://www.linkedin.com/in/igor-ruivo/";
+
+        /// <summary>
+        /// The current version of the AutoPatcher.
+        /// </summary>
+        public static readonly string CURRENT_VERSION = "v" + System.Diagnostics.FileVersionInfo.GetVersionInfo(System.Reflection.Assembly.GetExecutingAssembly().Location).FileVersion;
+
+        /// <summary>
+        /// The title of the main window.
+        /// </summary>
+        public static readonly string MAIN_WINDOW_TITLE = "M2Bob Patcher {0}";
+
+        /// <summary>
+        /// The M2bob launcher.
+        /// </summary>
+        public static readonly string M2BOB_STARTER = "your-exe.exe";
+
+        /// <summary>
+        /// The text description of the window triggered whenever there is already another instance of the AutoPatcher running.
+        /// </summary>
+        public static readonly string ALREADY_RUNNING = "Another instance of the AutoPatcher is already running.";
+
+        /// <summary>
+        /// The title of the window triggered whenever there is already another instance of the AutoPatcher running.
+        /// </summary>
+        public static readonly string ALREADY_RUNNING_ERROR = "Error Starting AutoPatcher";
+    }
+}
