6457b31e8941fc5b8a75ad4a398a8eed9aa13a10
igor-ruivo <igor.ruivo.97@gmail.com>
2024-01-15 23:35:58 +0000

    Change headers

--- GenericAutoUpdater/Downloaders/HttpClientDownloader.cs
+++ GenericAutoUpdater/Downloaders/HttpClientDownloader.cs
@@ -184,10 +184,9 @@ namespace GenericAutoUpdater.Downloaders {
         /// Sets up the default headers of the HttpClient to be used when performing HTTP operations.
         /// </summary>
         private static void SetupDefaultHeaders() {
-            HttpClient.DefaultRequestHeaders.Add("Accept", "text/html,application/xhtml+xml,application/xml");
-            HttpClient.DefaultRequestHeaders.Add("Accept-Encoding", "gzip, deflate");
-            HttpClient.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 6.2; WOW64; rv:19.0) Gecko/20100101 Firefox/19.0");
-            HttpClient.DefaultRequestHeaders.Add("Accept-Charset", "ISO-8859-1");
+            HttpClient.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36");
+            HttpClient.DefaultRequestHeaders.Add("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7");
+            HttpClient.DefaultRequestHeaders.Add("Accept-Encoding", "gzip, deflate, br");
         }
 
         /// <summary>
